387fa75a5ca71167c162af62c7b2e1c7e4673d54
Sharan Kumar  Kolluru <88401843+SharanKolluru@users.noreply.github.com>
2021-08-03 23:40:56 +0200

    Update GithubFinder.js

--- Github Finder/GithubFinder.js
+++ Github Finder/GithubFinder.js
@@ -1,14 +1,13 @@
 //Github class
 class GitHub{
-	constructor(){
-		this.client_id = '157219e35299ac60e486';
-		this.client_secret ='f15154886a12766645a72c41a53559c50d4a9bbd';
-		this.repos_count =5;
-		this.repos_sort ='created:asc';
+	constructor() {
+	    this.client_id = '157219e35299ac60e486';
+	    this.client_secret ='f15154886a12766645a72c41a53559c50d4a9bbd';
+	    this.repos_count =5;
+	    this.repos_sort ='created:asc';
 	}
 	//async getusers function
 	async getUsers(user){
-		
 		
 	const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 	const profile = await profileResponse.json();
